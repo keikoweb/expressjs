8a371e49fd28096578c586836156ef03f70ef8c3
keikoweb <keiko77mel@gmail.com>
2017-12-28 22:29:13 +1100

    add app.js

---
+++ app/app.js
@@ -0,0 +1,39 @@
+//expressjs
+var express = require('express');
+var app = express();
+var dataFile = require('./data/data.json');
+
+app.set('port', process.env.PORT || 3000 );
+app.get("/", function(req, res){
+  var info ='';
+  dataFile.speakers.forEach(function(item){
+    info += `
+    <li>
+      <h2>${item.name}</h2>
+      <p>${item.summary}</p>
+    </li>
+    `;
+  });
+  res.send(`
+      <h1>Roux Academy Meetups</h1>
+      ${info}
+  `);
+  res.send('<h1>Roux Academy Meetups</h1>');
+});
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Listening on port ' + app.get('port'));
+});
+
+// node.js
+// var http = require("http");
+//
+// var myServer = http.createServer(function(request, response)
+// {
+//     response.writeHead(200, {"Content-Type" : "text/html"});
+//     response.write("<h1>Roux Meetups</h1>");
+//     response.end();
+// });
+//
+// myServer.listen(3000);
+// console.log('Go to http://localhost:3000 on your browser');
