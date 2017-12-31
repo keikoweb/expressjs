fbc2ba031e4392a6e4ce2ef3de703c4e017f5ce3
keikoweb <keiko77mel@gmail.com>
2018-01-01 07:19:10 +1100

    Handling POST requests

--- app/public/js/feedback.js
+++ app/public/js/feedback.js
@@ -1,5 +1,14 @@
-$(function(){
+$(function() {
   $.getJSON('api', updateFeedback);
+
+  $('.feedback-form').submit(function(e) {
+    e.preventDefault();
+    $.post('api', {
+      name: $('#feedback-form-name').val(),
+      title: $('#feedback-form-title').val(),
+      message: $('#feedback-form-message').val()
+    }, updateFeedback);
+  });
 
   function updateFeedback(data) {
     var output = '';
@@ -8,7 +17,7 @@ $(function(){
       output += '  <div class="feedback-item media">';
       output += '    <div class="feedback-info media-body">';
       output += '       <div class="feedback-head">';
-      output += '         <div class="feedback-title">' + item.title + '<small class="feedback-name label label-info"></small></div>';
+      output += '         <div class="feedback-title">' + item.title + ' <small class="feedback-name label label-info">' + item.name + '</small></div>';
       output += '       </div>';
       output += '       <div class="feedback-message">' + item.message + '</div>';
       output += '    </div>';
